2f16d772876c5c00e09bfd645002f6edeef55a70
Jonathan Allen <jallen@snow.edu>
2020-01-11 22:31:23 -0700

    Get RLE parsing working!

--- lib/game_of_life/rle_parser.ex
+++ lib/game_of_life/rle_parser.ex
@@ -11,28 +11,23 @@ defmodule RleParser do
       lines: lines
     }
 
-    IO.inspect result
-
-    # cells = []
-    # row = result.x
-    # _col = result.y
-    # lines
-    # |> Enum.map_reduce(%{row: row, cells: cells}, fn line, acc ->
-    #   cells = parse_line(line, acc.row, acc.cells)
-    #   %{row: acc.row-1, cells: cells}
-    # end)
+    cells = []
+    row = result.y
+    {_row, cells} = lines
+      |> Enum.reduce({row, cells}, fn line, {row, cells} ->
+        cells = parse_line(line, row, cells)
+        {row - 1, cells}
+      end)
+    cells
   end
 
   def parse_line(line, row, cells) do
-    line_to_parts(line)
-    |> IO.inspect
-    |> Enum.flat_map_reduce(%{cells: cells, row: row}, fn part, acc ->
-      cells = add_cells(part, acc.row, 1, acc.cells)
-      IO.puts "Finished add_cells(#{inspect part}, #{row}, 1, #{inspect acc})"
-      acc = %{acc | row: acc.row-1, cells: cells}
-      acc
-      |> IO.inspect
-    end)
+    {cells, _row, _col} = line_to_parts(line)
+      |> Enum.reduce({cells, row, 1}, fn part, {cells, row, col} ->
+        %{cells: cells, col: new_col} = add_cells(part, row, col, cells)
+        {cells, row, new_col}
+      end)
+    cells
   end
 
   def line_to_parts(line) do
@@ -44,29 +39,18 @@ defmodule RleParser do
     end)
   end
 
-  def add_cells([], _row, col, cells) do
-    IO.puts "reached add_cells base case. col=#{col}, cells=#{inspect cells}"
-    %{cells: cells, col: col}
-  end
-
   def add_cells(command, row, col, cells) do
-    IO.puts "starting add_cells with #{inspect command}, #{row}, #{col}, #{inspect cells}"
     case Integer.parse(command) do
       {count, "o"} ->
         case count do
           0 ->
-            IO.puts "Down to 0, nothing more to add"
-            cells
+            %{cells: cells, col: col}
           _ ->
             cells = [%Cell{x: col, y: row} | cells]
-            IO.puts "Added new cell, calling with #{count-1}"
-            IO.inspect cells
             add_cells("#{count-1}o", row, col+1, cells)
         end
       {count, "b"} ->
-        IO.puts "I'm supposed to do #{count} blanks"
-        IO.puts "calling add_cells with [], #{row}, #{col+count}, #{inspect cells}"
-        add_cells([], row, col+count, cells)
+        %{cells: cells, col: col + count}
     end
   end
 end
--- test/game_of_life_test.exs
+++ test/game_of_life_test.exs
@@ -65,28 +65,38 @@ defmodule GameOfLifeTest do
       o$obo$2o!
       """
 
-      %{x: 3, y: 3, rule: "B3/S23", lines: lines} = RleParser.parse(rle)
-      inspect lines
+      actual = RleParser.parse(rle) |> Enum.sort
+      expected = [%Cell{x: 1, y: 3}, %Cell{x: 1, y: 2}, %Cell{x: 3, y: 2}, %Cell{x: 1, y: 1}, %Cell{x: 2, y: 1}] |> Enum.sort
+      assert expected == actual
     end
 
-    test "parse line - single cell on" do
-      actual = RleParser.parse_line("o", 3, 3, [])
-      expected = [%Cell{x: 1, y: 3}]
+    test "multiple blanks" do
+      rle = """
+      x = 9, y = 1, rule = B3/S23
+      o2bo4bo!
+      """
 
+      actual = RleParser.parse(rle) |> Enum.sort
+      expected = [
+        %Cell{x: 1, y: 1}, %Cell{x: 4, y: 1},%Cell{x: 9, y: 1}
+        ] |> Enum.sort
       assert expected == actual
     end
   end
 
 end
 
-"""
+_sample = """
 x = 24, y = 35, rule = B3/S23
 o$o$o$o$o$o10b7o$bo7b2o3b2ob2o$b2o5b2o2b5ob2o$2b2o3bo3b2o4bobo$3bob2o
 3b2o6b2o$4b2o3bo5b5o$3b3o2b2o4b2o3bo$2b2o2b3o4bo5bo$bo5b2o4bo5bo$2o5b
 3o2b2o4bo$o6bo2bobo4b2o$o6bo3b6o$o6bo4bo$o6bo3b2o$o6bo3bo$3o4bo3bo$2b
 21o$8bo2bo11bo$8bo2bo11bo$9b3o11bo$10b2o10b2o$11b4o5b3o$12bo2b5o$12bo$
 12bo$12bo$12bo$12bo$12bo$12bo!
-
-
-
+"""
+_sample2 = """
+x = 24, y = 19, rule = B3/S23
+$4bo9b2o$4b3o8b2o$6b2o8b2o$7bo9bo$7b2o8b2o4bo$8bo14bo$23bo$o22bo$o22bo
+$o21b2o$o21bo$o20b2o$2o18b2o$b2o16b2o$2b2o13b3o$3b2o10b3o$4b2o4b5o$5b
+6o!
 """
